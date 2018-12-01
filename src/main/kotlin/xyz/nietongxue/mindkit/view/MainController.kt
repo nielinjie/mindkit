7f6e2c98790f2aaae98d933aa2d67efcf340c84d
nielinjie <nielinjie@gmail.com>
2018-12-01 23:54:43 +0800

    初步实现plugin。结构不太理想，需要重构。

--- src/main/kotlin/xyz/nietongxue/mindkit/view/MainController.kt
+++ src/main/kotlin/xyz/nietongxue/mindkit/view/MainController.kt
@@ -5,7 +5,7 @@ import xyz.nietongxue.mindkit.model.Node
 import xyz.nietongxue.mindkit.application.marpPPT.ProcessorController
 
 class MainController : Controller() {
-
+//TODO 这里需要持有node，而不是每次传进来。以便能在node不变、process变化的时候再运行一次process
     var processorController:xyz.nietongxue.mindkit.application.Controller?=null
 
     fun process(node: Node) {
