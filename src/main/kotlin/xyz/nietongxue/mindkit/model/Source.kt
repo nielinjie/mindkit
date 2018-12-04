052a0f38e110dc99daa373a0a90704c5604e3cd7
nielinjie <nielinjie@gmail.com>
2018-12-04 20:36:06 +0800

    source方面的想法

---
+++ src/main/kotlin/xyz/nietongxue/mindkit/model/Source.kt
@@ -0,0 +1,10 @@
+package xyz.nietongxue.mindkit.model
+
+//TODO 多文件
+//TODO 一个目录作为home（repository）
+
+//TODO 多类型
+
+/*TODO filter？或者合并在类型里面 filter貌似应该是app级别的，有些app关注这些节点，有些app关注哪些节点？
+   filter跟processor如何配合？apply一个app将应用多个filter 和多个 processor？
+ */
