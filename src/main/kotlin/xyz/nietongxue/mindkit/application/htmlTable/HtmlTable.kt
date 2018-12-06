67f681030fded8a3a0f58b7cc440e287ed17da3c
nielinjie <nielinjie@gmail.com>
2018-12-06 20:26:18 +0800

    htmltable设计。

---
+++ src/main/kotlin/xyz/nietongxue/mindkit/application/htmlTable/HtmlTable.kt
@@ -0,0 +1,18 @@
+package xyz.nietongxue.mindkit.application.htmlTable
+
+import xyz.nietongxue.mindkit.application.AppDescriptor
+import xyz.nietongxue.mindkit.application.Controller
+import xyz.nietongxue.mindkit.model.Processor
+
+class HtmlTable : AppDescriptor{
+    //TODO xmind  里面的table，生成html（或者markdown？）table，比如可以copy到conf
+    override val name: String
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+    override val description: String
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+    override val providedProcessors: List<Processor>
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+    override val controller: Controller
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+
+}
