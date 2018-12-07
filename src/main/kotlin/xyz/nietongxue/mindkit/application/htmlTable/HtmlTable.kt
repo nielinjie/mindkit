3d4eec78db8277fd1ecde40aee1d285304124e67
nielinjie <nielinjie@gmail.com>
2018-12-07 16:38:02 +0800

    htmltable设计。

--- src/main/kotlin/xyz/nietongxue/mindkit/application/htmlTable/HtmlTable.kt
+++ src/main/kotlin/xyz/nietongxue/mindkit/application/htmlTable/HtmlTable.kt
@@ -6,6 +6,10 @@ import xyz.nietongxue.mindkit.model.Processor
 
 class HtmlTable : AppDescriptor{
     //TODO xmind  里面的table，生成html（或者markdown？）table，比如可以copy到conf
+    // - 生成html可以（至少可以以safari打开，然后copy到conf editor）
+    // 后续看看是否可以展示在fx里，然后copy到clipboard。
+
+
     override val name: String
         get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
     override val description: String
