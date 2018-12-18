8314da63d7de8deb3af3080b7e5be268fe93627a
nielinjie <nielinjie@gmail.com>
2018-12-18 21:50:14 +0800

    尝试用popover加grideview/table来代替combobox。

--- src/main/kotlin/xyz/nietongxue/mindkit/view/SourceView.kt
+++ src/main/kotlin/xyz/nietongxue/mindkit/view/SourceView.kt
@@ -3,10 +3,13 @@ package xyz.nietongxue.mindkit.view
 import javafx.beans.property.SimpleObjectProperty
 import javafx.event.ActionEvent
 import javafx.event.EventHandler
+import javafx.scene.control.Button
+import javafx.scene.control.Label
 import javafx.scene.control.TreeItem
 import javafx.scene.layout.Priority
 import javafx.scene.layout.VBox
 import javafx.util.StringConverter
+import org.controlsfx.control.PopOver
 import tornadofx.*
 import xyz.nietongxue.mindkit.model.Favorite
 import xyz.nietongxue.mindkit.model.Favorites
@@ -19,29 +22,39 @@ class SourceView : View() {
     val favoriteP = SimpleObjectProperty<Favorite>(Favorites.all[0])
 
 
+
     init {
+        val popover = PopOver(Button("button"))
         with(root) {
-            this += combobox(favoriteP, Favorites.all) {
-                isEditable = false
-                this.converter = object : StringConverter<Favorite>() {
-                    override fun toString(`object`: Favorite?): String {
-                        return `object`?.name!!
-                    }
+            //            this += combobox(favoriteP, Favorites.all) {
+//                isEditable = false
+//                this.converter = object : StringConverter<Favorite>() {
+//                    override fun toString(`object`: Favorite?): String {
+//                        return `object`?.name!!
+//                    }
+//
+//                    override fun fromString(string: String?): Favorite {
+//                        throw IllegalStateException("Can not edit")
+//                    }
+//
+//                }
+//                this.onAction = EventHandler<ActionEvent> {
+//                    //NOTE 代替是favorite的行为，而不是source的，所以source是append
+//                    treeModel.root.removeChildren()
+//                    treeModel.mount(this.value.sources())
+//                }
+//                treeModel.root.removeChildren()
+//                treeModel.mount(this.value.sources())
+//
+//            }
 
-                    override fun fromString(string: String?): Favorite {
-                        throw IllegalStateException("Can not edit")
-                    }
+            button("popover") {
 
+                action {
+                    popover.show(this)
                 }
-                this.onAction = EventHandler<ActionEvent> {
-                    //NOTE 代替是favorite的行为，而不是source的，所以source是append
-                    treeModel.root.removeChildren()
-                    treeModel.mount(this.value.sources())
-                }
-                treeModel.root.removeChildren()
-                treeModel.mount(this.value.sources())
-
             }
+
             scrollpane {
                 isFitToHeight = true
                 isFitToWidth = true
@@ -59,11 +72,13 @@ class SourceView : View() {
                         it.value.children
                     }
                 }
-            }
 
+            }
         }
 
-
     }
+
+
 }
+
 
