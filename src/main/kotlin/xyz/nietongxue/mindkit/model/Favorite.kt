8a2b01a2def7b98be13c33bde552322af9362f8c
nielinjie <nielinjie@gmail.com>
2019-01-13 20:27:52 +0800

    打开文件夹

--- src/main/kotlin/xyz/nietongxue/mindkit/model/Favorite.kt
+++ src/main/kotlin/xyz/nietongxue/mindkit/model/Favorite.kt
@@ -1,8 +1,12 @@
 package xyz.nietongxue.mindkit.model
 
+import com.beust.klaxon.*
+import org.reflections.serializers.JsonSerializer
+import tornadofx.Component
 import xyz.nietongxue.mindkit.application.xmind.XMindFavorite
 import xyz.nietongxue.mindkit.source.FolderSource
 import xyz.nietongxue.mindkit.source.Source
+import kotlin.reflect.KClass
 
 
 interface Favorite {
@@ -10,7 +14,7 @@ interface Favorite {
     //NOTE 相对于source，favorite是个比较上层和局部的概念，不涉及核心逻辑。
     fun sources(): List<Source>
 
-    val name: String
+    fun name(): String
 }
 
 data class FolderFavorite(val path: String) : Favorite {
@@ -18,15 +22,52 @@ data class FolderFavorite(val path: String) : Favorite {
         return listOf(FolderSource(path))
     }
 
-    override val name = "Folder - $path"
+    override fun name() = "Folder - $path"
 }
 
-object Favorites {
-    val all: List<Favorite> = listOf(
+object Favorites : Component() {
+    val all: MutableList<Favorite> = mutableListOf()
+    val default: List<Favorite> = listOf(
             XMindFavorite("/Users/nielinjie/Desktop/ppt.xmind")
             ,
             XMindFavorite("/Users/nielinjie/Desktop/19年计划.xmind"),
             FolderFavorite("/Users/nielinjie/Desktop"),
             FolderFavorite("/Users/nielinjie/Library/Mobile Documents/com~apple~CloudDocs/思考和写作")
     )
+
+    init {
+        val jsonS: String = (config["favorites"] as? String) ?: "[]"
+
+
+        val favorites: List<Favorite> = (Parser.default().parse(jsonS.reader()) as JsonArray<JsonObject>).map {
+            val t = it.string("_type")!!
+            val ob = it.obj("favorite")!!
+            when (t) {
+                "xyz.nietongxue.mindkit.model.FolderFavorite" -> Klaxon().parseFromJsonObject<FolderFavorite>(ob)
+                "xyz.nietongxue.mindkit.application.xmind.XMindFavorite" -> Klaxon().parseFromJsonObject<XMindFavorite>(ob)
+                else -> throw IllegalArgumentException("Unknown type: $t")
+            }
+        }.filterNotNull()
+        all.addAll(if (favorites.isEmpty()) default else favorites)
+
+
+    }
+
+    fun add(favorite: Favorite) {
+        all.add(favorite)
+    }
+
+    fun save() {
+        data class FavoriteData(
+                val _type: String,
+                val favorite: Favorite)
+
+        val list: List<FavoriteData> = all.map { FavoriteData(it.javaClass.name, it) }
+        val jsonS = Klaxon().toJsonString(list)
+        config["favorites"] = jsonS
+        config.save()
+    }
 }
+
+
+
--- src/main/kotlin/xyz/nietongxue/mindkit/view/FavoriteView.kt
+++ src/main/kotlin/xyz/nietongxue/mindkit/view/FavoriteView.kt
@@ -1,6 +1,8 @@
 package xyz.nietongxue.mindkit.view
 
+import javafx.beans.property.SimpleListProperty
 import javafx.beans.property.SimpleObjectProperty
+import javafx.collections.ObservableList
 import javafx.event.ActionEvent
 import javafx.event.EventHandler
 import javafx.scene.layout.VBox
@@ -8,14 +10,18 @@ import javafx.util.StringConverter
 import org.controlsfx.control.PopOver
 import tornadofx.Component
 import tornadofx.combobox
+import tornadofx.observable
 import xyz.nietongxue.mindkit.model.Favorite
 import xyz.nietongxue.mindkit.model.Favorites
+import xyz.nietongxue.mindkit.model.FolderFavorite
 import xyz.nietongxue.mindkit.util.defaultPadding
 import java.io.File
 
 class FavoriteView : Component() {
     val popoverContent = VBox()
-    val favoriteP = SimpleObjectProperty<Favorite>(Favorites.all[0])
+    val allFavoriteP = SimpleListProperty<Favorite>(Favorites.all.observable())
+
+    val favoriteP = SimpleObjectProperty<Favorite>(allFavoriteP.value.first())
     var favoriteSelected: ((Favorite) -> Unit)? = null
         set(value) {
             field = value
@@ -23,19 +29,22 @@ class FavoriteView : Component() {
         }
     var popOver: PopOver
 
+    
+
+
     init {
         val currentFavoriteName = config["currentFavoriteName"] as? String
         favoriteP.value = Favorites.all.find {
-            it.name == currentFavoriteName
+            it.name() == currentFavoriteName
         } ?: Favorites.all.first()
 
         with(popoverContent) {
             defaultPadding()
-            combobox(favoriteP, Favorites.all) {
+            combobox(favoriteP, allFavoriteP) {
                 isEditable = false
                 this.converter = object : StringConverter<Favorite>() {
                     override fun toString(`object`: Favorite?): String {
-                        return `object`?.name!!
+                        return `object`?.name()!!
                     }
 
                     override fun fromString(string: String?): Favorite {
@@ -56,13 +65,23 @@ class FavoriteView : Component() {
         }
 
     }
+
     //TODO 找个该在的地方
-    fun addFolder(folder: File){
-        println(folder.absolutePath)
+    fun addFolder(folder: File) {
+        val folderFavorite = FolderFavorite(folder.absolutePath)
+        val findFavorite = Favorites.all.find { it is FolderFavorite && it.path == folderFavorite.path }
+        if (findFavorite == null) {
+            Favorites.add(folderFavorite)
+//            this.allFavoriteP.add(folderFavorite)
+        }
+        this.favoriteP.set(folderFavorite)
+        //TODO 整理，可能应该放到onChange里面
+        favoriteSelected?.also { it(this.favoriteP.value) }
     }
 
     fun onClose() {
-        config["currentFavoriteName"] = favoriteP.value.name
+        Favorites.save()
+        config["currentFavoriteName"] = favoriteP.value.name()
         config.save()
     }
 }
